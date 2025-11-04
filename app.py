7b2d53dfbede7305fda148c2616a0dd01ba4be43
AxelPrince8 <honer.irtaral@gmail.com>
2025-11-04 14:00:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -149,4 +149,5 @@ def uploaded_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    import os
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
